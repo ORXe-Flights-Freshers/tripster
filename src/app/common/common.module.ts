bb8226f7caa9fcc0bde444820a852c4d5b189566
Gupta <pgupta@internal.tavisca.com>
2019-10-17 16:28:26 +0530

    Refactored and debug the code

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from "@angular/core";
 import { HeaderComponent } from "./components/header/header.component";
 import { PlaceAutocompleteComponent } from "./components/place-autocomplete/place-autocomplete.component";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [HeaderComponent, PlaceAutocompleteComponent],
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
+  ],
   exports: [HeaderComponent, PlaceAutocompleteComponent]
 })
 export class CommonModule {}
