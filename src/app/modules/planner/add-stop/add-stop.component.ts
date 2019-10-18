b8280029ca1c37e1458cf65bfea25d3736d8bb27
tavisca-sverma <sverma@tavisca.in>
2019-10-18 17:01:04 +0530

    Create empty add-stop component

---
+++ src/app/modules/planner/add-stop/add-stop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-stop',
+  templateUrl: './add-stop.component.html',
+  styleUrls: ['./add-stop.component.css']
+})
+export class AddStopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/planner/planner.module.ts
+++ src/app/modules/planner/planner.module.ts
@@ -3,6 +3,7 @@ import { SharedModule } from "../../shared/shared.module";
 import { PlannerComponent } from "./planner/planner.component";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
+import { AddStopComponent } from './add-stop/add-stop.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [PlannerComponent],
+  declarations: [PlannerComponent, AddStopComponent],
   imports: [SharedModule, CommonModule, RouterModule.forChild(routes)]
 })
 export class PlannerModule {}
