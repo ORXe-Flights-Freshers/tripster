fabea2758c8733a9ae58e63323e45c841a1353d0
tavisca-vraut <vraut@tavisca.in>
2019-10-18 13:05:57 +0530

    modify header for less than 319px width

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { AgmCoreModule } from "@agm/core";
-import { AppComponent } from "./app.component";
-import { HomePageModule } from "./modules/home-page/home-page.module";
-import { AgmDirectionModule } from "agm-direction";
-import { CommonModule } from "./common/common.module";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+import { HomePageModule } from './modules/home-page/home-page.module';
+import { AgmDirectionModule } from 'agm-direction';
+import { CommonModule as TripCommonModule } from './common/common.module';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AgmCoreModule.forRoot({
-      apiKey: "AIzaSyC2LnC7a1z5MDzBjx4Us9qo9Z4Yupum03A",
-      libraries: ["places"]
+      apiKey: 'AIzaSyC2LnC7a1z5MDzBjx4Us9qo9Z4Yupum03A',
+      libraries: ['places']
     }),
     HomePageModule,
     HttpClientModule,
     AgmDirectionModule,
-    CommonModule,
+    TripCommonModule,
     BrowserModule
   ],
   providers: [],
