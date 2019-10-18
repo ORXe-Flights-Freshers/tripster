9a6e7ac6c3ca963e97ccf64bcb28b82436baf3b7
Gupta <pgupta@internal.tavisca.com>
2019-10-18 09:17:48 +0530

    Add planner module

---
+++ src/app/modules/planner/planner.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CommonModule as TripCommonModule } from "../../common/common.module";
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, TripCommonModule]
+})
+export class PlannerModule {}
