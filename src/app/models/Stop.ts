9854d626a80e3259ed392346cbf5b0c61775512c
Gupta <pgupta@internal.tavisca.com>
2019-10-21 00:29:42 +0530

    Update stop class in model

--- src/app/models/Stop.ts
+++ src/app/models/Stop.ts
@@ -1,1 +1,12 @@
-export class Stop {}
+import { Location } from "./Location";
+import { PlaceAutocompleteComponent } from "../shared/components/place-autocomplete/place-autocomplete.component";
+import { Place } from "./Place";
+
+export class Stop {
+  stopId: Number;
+  name: String;
+  location: Location;
+  arrival: Date;
+  departure: Date;
+  places: Place[];
+}
