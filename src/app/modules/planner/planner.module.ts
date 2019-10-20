a1f0f7907be202c6a649cd5c103544a13e7a796f
Gupta <pgupta@internal.tavisca.com>
2019-10-20 18:07:28 +0530

    Add Map Component

--- src/app/modules/planner/planner.module.ts
+++ src/app/modules/planner/planner.module.ts
@@ -3,6 +3,7 @@ import { SharedModule } from "../../shared/shared.module";
 import { PlannerComponent } from "./planner/planner.component";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
+import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [PlannerComponent],
+  declarations: [PlannerComponent, MapComponent],
   imports: [SharedModule, CommonModule, RouterModule.forChild(routes)]
 })
 export class PlannerModule {}
