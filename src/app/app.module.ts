ca186d5371087c6917a3315212c3dc82b52f7fad
tavisca-vraut <vraut@tavisca.in>
2019-10-22 10:37:53 +0530

    fix linting errors/warnings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { AgmCoreModule } from "@agm/core";
-import { AppComponent } from "./app.component";
-import { AgmDirectionModule } from "agm-direction";
-import { SharedModule } from "./shared/shared.module";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+import { AgmDirectionModule } from 'agm-direction';
+import { SharedModule } from './shared/shared.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlannerModule } from './modules/planner/planner.module';
 
 @NgModule({
