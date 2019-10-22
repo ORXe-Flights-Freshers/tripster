38cbb39af6e8f16fd63dc36399754237f132d60e
tavisca-vraut <vraut@tavisca.in>
2019-10-22 12:00:02 +0530

    remove redundant directive

--- src/app/modules/home-page/time-picker-theme/time-picker-theme.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimePickerThemeDirective } from './time-picker-theme.directive';
-
-describe('TimePickerThemeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TimePickerThemeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
