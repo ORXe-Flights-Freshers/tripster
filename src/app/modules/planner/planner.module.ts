9aabfec1d288b79940910b3cf6a7ad1032c27947
tavisca-vraut <vraut@tavisca.in>
2019-10-25 12:43:55 +0530

    fix linting errors

--- src/app/modules/planner/planner.module.ts
+++ src/app/modules/planner/planner.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from "@angular/core";
-import { SharedModule } from "../../shared/shared.module";
-import { PlannerComponent } from "./planner/planner.component";
-import { CommonModule } from "@angular/common";
-import { Routes, RouterModule } from "@angular/router";
-import { MapComponent } from "./map/map.component";
-import { TimelineComponent } from "./timeline/timeline.component";
-import { AgmDirectionModule } from "agm-direction";
-import { AgmCoreModule } from "@agm/core";
-import { AppMaterialModule } from "../app-material/app-material.module";
-import { AddStopComponent } from "./add-stop/add-stop.component";
-import { HotelCardComponent } from "./hotel-card/hotel-card.component";
-import { HotelCardListComponent } from "./hotel-card-list/hotel-card-list.component";
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { PlannerComponent } from './planner/planner.component';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './map/map.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { AgmDirectionModule } from 'agm-direction';
+import { AgmCoreModule } from '@agm/core';
+import { AppMaterialModule } from '../app-material/app-material.module';
+import { AddStopComponent } from './add-stop/add-stop.component';
+import { HotelCardComponent } from './hotel-card/hotel-card.component';
+import { HotelCardListComponent } from './hotel-card-list/hotel-card-list.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const routes: Routes = [
   {
-    path: ":id",
+    path: ':id',
     component: PlannerComponent
   },
   {
-    path: "add",
+    path: 'add',
     component: AddStopComponent
   }
 ];
@@ -35,8 +35,8 @@ const routes: Routes = [
   ],
   imports: [
     AgmCoreModule.forRoot({
-      apiKey: "AIzaSyC2LnC7a1z5MDzBjx4Us9qo9Z4Yupum03A",
-      libraries: ["places"]
+      apiKey: 'AIzaSyC2LnC7a1z5MDzBjx4Us9qo9Z4Yupum03A',
+      libraries: ['places']
     }),
     AgmDirectionModule,
     SharedModule,
