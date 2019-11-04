35df0423f36f943e384337069eacfbdeb3f41544
tavisca-sverma <sverma@tavisca.in>
2019-11-04 14:42:05 +0530

    Update departue date based on arrival date of stop

--- src/app/modules/planner/map/map.component.ts
+++ src/app/modules/planner/map/map.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { TripService } from 'src/app/services/trip.service';
+import { Trip } from 'src/app/models/Trip';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-  constructor(public tripService: TripService) {
-    console.log(this.tripService.trip);
-  }
 
-  ngOnInit() {}
+
+export class MapComponent implements OnInit {
+  isLoading: boolean;
+  constructor(public tripService: TripService) {
+    // console.log(this.tripService.trip);
+    this.isLoading = true;
+  }
+  origin;
+  destination;
+
+  markerOptions={
+    // origin:{
+    //   icon: 'https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi2.png',
+    // },
+    // destination:{
+    //   icon: 'https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi2.png',
+    // },
+    waypoints: {
+      icon: 'https://i.imgur.com/7teZKif.png',
+  },
+  };
+  
+  ngOnInit() {
+    this.tripService.tripSubject.pipe(take(1)).subscribe((trip: Trip) => {
+      this.tripService.updateWaypoints();
+      this.origin={
+        lat: this.tripService.trip.source.location.latitude,
+        lng: this.tripService.trip.source.location.longitude
+      };
+      this.destination={
+        lat: this.tripService.trip.destination.location.latitude,
+        lng: this.tripService.trip.destination.location.longitude
+      };
+
+
+      this.isLoading = false;  
+    });
+  }
 }
