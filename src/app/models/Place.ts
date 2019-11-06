316a7f19fbac2167432a7b8feafc0d4160525881
Gupta <pgupta@internal.tavisca.com>
2019-11-06 12:16:15 +0530

    Update model

--- src/app/models/Place.ts
+++ src/app/models/Place.ts
@@ -1,11 +1,11 @@
-import { Location } from './Location';
+import { Location } from "./Location";
 
 export interface Place {
   placeId: string;
   name: string;
   description: string;
   location: Location;
-  placeType: string;
   arrival: string;
   departure: string;
+  rating: number;
 }
