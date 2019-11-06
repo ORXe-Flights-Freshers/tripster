75aa1f8f40face11de59f073bcbe2916ca54df83
Gupta <pgupta@internal.tavisca.com>
2019-11-06 13:09:32 +0530

    Add dist files

--- src/app/modules/planner/planner/planner.component.ts
+++ src/app/modules/planner/planner/planner.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { TripService } from 'src/app/services/trip.service';
-import { HttpClient } from '@angular/common/http';
-import { Trip } from 'src/app/models/Trip';
-import {
-  MatDialog,
-  MatDialogConfig
-} from '@angular/material/dialog';
-import {HeaderDataService} from '../../../services/HeaderData/header-data.service';
-import { AddHotelDetailsComponent } from '../add-hotel-details/add-hotel-details.component';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { TripService } from "src/app/services/trip.service";
+import { HttpClient } from "@angular/common/http";
+import { Trip } from "src/app/models/Trip";
+import { HeaderDataService } from "../../../services/HeaderData/header-data.service";
 
 @Component({
-  selector: 'app-planner',
-  templateUrl: './planner.component.html',
-  styleUrls: ['./planner.component.css']
+  selector: "app-planner",
+  templateUrl: "./planner.component.html",
+  styleUrls: ["./planner.component.css"]
 })
 export class PlannerComponent implements OnInit {
   activeTab: string;
@@ -22,8 +17,8 @@ export class PlannerComponent implements OnInit {
     public route: ActivatedRoute,
     public tripService: TripService,
     private http: HttpClient,
-    public dialog: MatDialog,
-    private headerLinks: HeaderDataService) {
+    private headerLinks: HeaderDataService
+  ) {
     this.headerLinks.customizeHeaderForPlannerPage();
   }
 
@@ -34,31 +29,10 @@ export class PlannerComponent implements OnInit {
   ngOnInit() {
     // @ts-ignore
     const id = this.route.params.value.id;
-    this.http.get('http://3.14.69.62:5000/api/trip/' + id)
-      .subscribe(data => {
-        this.tripService.trip = data as Trip;
-        this.tripService.tripSubject.next(this.tripService.trip);
-      });
-    this.activeTab = 'timeline';
-  }
-
-  closeHotelDialog() {
-    this.dialog.closeAll();
-  }
-
-  openHotelDialog(hotelParentData): void {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = '400px';
-    dialogConfig.height = '510px';
-
-    dialogConfig.data = hotelParentData;
-    const dialogRef = this.dialog.open(AddHotelDetailsComponent, dialogConfig);
-
-    dialogRef.afterClosed()
-      .subscribe(placeFromDialog => {
-        console.log(placeFromDialog);
-        if (placeFromDialog) {}
-      });
+    this.http.get("http://3.14.69.62:5000/api/trip/" + id).subscribe(data => {
+      this.tripService.trip = data as Trip;
+      this.tripService.tripSubject.next(this.tripService.trip);
+    });
+    this.activeTab = "timeline";
   }
 }
