53bf7b4dcacc7350351ccd49fa0205a048ea8530
tavisca-vraut <vraut@tavisca.in>
2019-11-07 09:20:34 +0530

    add the page-not-found page

---
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {HeaderDataService} from '../../../services/HeaderData/header-data.service';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor(private headerDataService: HeaderDataService) {
+    this.headerDataService.customizeHeaderForNotFoundPage();
+  }
+
+  ngOnInit() {
+  }
+
+}
