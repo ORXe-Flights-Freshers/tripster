6ffb1e141edec6ab70aea1c3ea329c1b0852446c
tavisca-vraut <vraut@tavisca.in>
2019-11-25 10:33:57 +0530

    add popular trips component

--- src/app/modules/home-page/popular-trip/popular-trip.component.ts
+++ src/app/modules/home-page/popular-trip/popular-trip.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PopularTripService } from '@services/popular-trip.service';
+
 
 @Component({
   selector: 'app-popular-trip',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popular-trip.component.css']
 })
 export class PopularTripComponent implements OnInit {
+  source = 'Mumbai';
+  destination = 'Goa';
 
-  constructor() { }
+  lat: number;
+  lng: number;
+  imageUrl = 'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+
+  constructor(private popularTrip: PopularTripService) { }
 
   ngOnInit() {
+    // this.imageUrl = this.popularTrip.getPlaceImageUrl(this.lat, this.lng);
   }
 
 }
