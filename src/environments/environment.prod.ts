b58ec670a8a75ee22c83bc027f81a42d8b72bd58
tavisca-vraut <vraut@tavisca.in>
2019-12-02 16:01:43 +0530

    add proper maps api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
          production: true,
          baseUrl: 'http://3.14.69.62',
          port: 5000,
-         mapsApiKey: 'AIzaSyCESD07xxZcABiJZ9Aa5mB3I9nSLcCM3Nc',
+         mapsApiKey: 'AIzaSyBktK-V9fFKUgqD41fHZUosV4L86YK247c',
          googleClientId: '1047010521344-45llkt5qr16ftg6m26j4priocs1ke487.apps.googleusercontent.com'
        };
